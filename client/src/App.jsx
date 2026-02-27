48433f7867b607dd46e03acb8735dbd7744a7a4b
Aziz Zaghdoudi <aziz@aims.team>
2026-02-27 03:22:21 -0500

    Phase 2 : init Phase 2 : setup vite/react + tailwindcss

---
+++ client/src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css';
+
+
+
+function App() {
+  return (
+    <div className='min-h-screen bg-gray-900 text-white'>
+      <h1 className='text-3xl font-bold text-center p-8'>
+        JobMatch AI
+      </h1>
+    </div>
+
+  );
+
+
+}
+
+
+export default App
